f2439324b959b73acb1905a9c70f180531c75e8a
Igor <Sketch_90@mail.ru>
2023-04-19 10:09:15 +0300

    added for deploy settings

--- src/main/java/com/example/thebest_youdo/config/ApplicationConfig.java
+++ src/main/java/com/example/thebest_youdo/config/ApplicationConfig.java
@@ -5,11 +5,11 @@ import com.example.thebest_youdo.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -20,6 +20,7 @@ import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @RequiredArgsConstructor
+@PropertySource("classpath:application.properties")
 public class ApplicationConfig {
 
     private final UserRepository repository;
